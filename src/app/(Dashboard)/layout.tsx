728988b5b7fcb7f4b7c54a30cd2a9558e04c2297
Sunday Etom Eni <sundaywht@gmail.com>
2025-04-12 20:41:23 +0100

    concluded

--- src/app/(Dashboard)/layout.tsx
+++ src/app/(Dashboard)/layout.tsx
@@ -1,13 +1,9 @@
-// import Image from "next/image";
-
 import Box from '@mui/material/Box';
 // import colors from '@/constants/colors';
 import Stack from '@mui/material/Stack';
 // import Typography from '@mui/material/Typography';
 import SideNav from "@/components/SideNav";
 import Header from '@/components/Header';
-import OuterRightSideNav from '@/components/OuterRightSideNav';
-// import Greeting from '@/components/Greeting';
 
 
 export default function Layout({
@@ -16,22 +12,20 @@ export default function Layout({
 	children: React.ReactNode;
 }>) {
 
-
-
 	return (
 		<Box>
 			<Stack flexDirection="row">
-				<SideNav currentScreen="Dashboard" />
+				<Box>
+					<SideNav />
+				</Box>
 
-				<Box sx={{ flex: 1 }}>
+				<Box sx={{ flexGrow: 1 }}>
 					<Header fullName='Sunday Etom' />
 
 					<Box>
 						{children}
 					</Box>
 				</Box>
-
-				{/* <OuterRightSideNav currentScreen="Dashboard" /> */}
 			</Stack>
 
 		</Box>
--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import React from 'react';
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
 
 import Box from '@mui/material/Box';
 // import colors from '@/constants/colors';
@@ -19,25 +22,29 @@ interface _Props {
     // children: React.ReactNode,
     // bottomSpacing?: number,
     // topSpacing?: boolean,
-    currentScreen: string
+    // currentScreen: string
 }
 
-export default function SideNav({ currentScreen = "Dashboard" }: _Props) {
+// export default function SideNav({ currentScreen = "Dashboard" }: _Props) {
+export default function SideNav() {
+    const pathname = usePathname();
+
+    
     const sideNavItem = [
         {
-            isActive: currentScreen == "Dashboard" ? true : false,
+            isActive: pathname.length < 2 ? true : false,
             title: "Dashboard",
             link: "/",
             icon: <HomeOutlinedIcon sx={{ fontSize: "18px" }} />,
         },
         {
-            isActive: currentScreen == "Profile" ? true : false,
+            isActive: pathname.includes("/Profile") ? true : false,
             title: "Profile",
             link: "/Profile",
             icon: <PersonOutlineOutlinedIcon sx={{ fontSize: "18px" }} />
         },
         {
-            isActive: currentScreen == "Settings" ? true : false,
+            isActive: pathname.includes("/Settings") ? true : false,
             title: "Settings",
             link: "/Settings",
             icon: <SettingsOutlinedIcon sx={{ fontSize: "18px" }} />
@@ -48,42 +55,95 @@ export default function SideNav({ currentScreen = "Dashboard" }: _Props) {
     return (
         <Box
             sx={{
-                px: "15px",
                 width: "237px",
-                height: "100dvh",
-                // bgcolor: "gray",
-                display: "flex",
-                flexDirection: "column",
-                gap: "20"
             }}
         >
-            <Stack direction="row" alignItems='center' height="80px">
-                <Image
-                    // className="dark:invert"
-                    src="/Logo.png"
-                    alt="logo"
-                    width={73}
-                    height={19}
-                    priority
-                />
-            </Stack>
-            
-            <Box my={"15px"}>
-                {sideNavItem.map((nav, index) => (
-                    <Link href={nav.link} key={index}>
+            <Box
+                sx={{
+                    px: "15px",
+                    width: "230px",
+                    height: "100dvh",
+                    bgcolor: "#fff",
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "20",
+                    // position: "static",
+                    // top: 0
+
+                    position: "fixed",
+                    top: 0,
+                    overflowY: "scroll"
+                }}
+            >
+                <Stack direction="row" alignItems='center' height="80px">
+                    <Image
+                        // className="dark:invert"
+                        src="/Logo.png"
+                        alt="logo"
+                        width={73}
+                        height={19}
+                        priority
+                    />
+                </Stack>
+                
+                <Box my={"15px"}>
+                    {sideNavItem.map((nav, index) => (
+                        <Link href={nav.link} key={index}>
+                            <Stack direction="row" alignItems='center'  gap={"5px"}
+                                sx={{
+                                    borderRadius: "14px",
+                                    padding: "16px",
+                                    mb: "5px",
+                                    bgcolor: nav.isActive ? "#19A752" : "",
+                                    color: nav.isActive ? "#fff" : '#434343',
+                                    ":hover": {
+                                        bgcolor: nav.isActive ? "" : "#19A75210",
+                                    }
+                                }}
+                            >
+                                { nav.icon }
+
+                                <Typography
+                                    sx={{
+                                        // fontFamily: "Libre Franklin",
+                                        fontWeight: "400",
+                                        fontSize: "16px",
+                                        lineHeight: "100%",
+                                        letterSpacing: "0%"
+                                    }}				
+                                >{nav.title}</Typography>
+                            </Stack>
+                        </Link>
+                    ))}
+                </Box>
+
+                <Box my="auto">
+                    <Typography mb={2}
+                        sx={{
+                            // fontFamily: "Libre Franklin",
+                            fontWeight: "600",
+                            fontSize: "16px",
+                            lineHeight: "100%",
+                            letterSpacing: "0%",
+                            // color: "#000"
+                        }}				
+                    >Help & Support</Typography>
+
+                    <Link href="/FAQs">
                         <Stack direction="row" alignItems='center'  gap={"5px"}
                             sx={{
                                 borderRadius: "14px",
                                 padding: "16px",
                                 mb: "5px",
-                                bgcolor: nav.isActive ? "#19A752" : "",
-                                color: nav.isActive ? "#fff" : '#434343',
+
+                                bgcolor: pathname.includes("/FAQs") ? "#19A752" : "",
+                                color: pathname.includes("/FAQs") ? "#fff" : '#434343',
                                 ":hover": {
-                                    bgcolor: nav.isActive ? "" : "#19A75210",
+                                    bgcolor: pathname.includes("/FAQs") ? "" : "#19A75210",
                                 }
                             }}
                         >
-                            { nav.icon }
+                            <QuestionMarkOutlinedIcon sx={{ color: "18px" }} />
 
                             <Typography
                                 sx={{
@@ -93,83 +153,42 @@ export default function SideNav({ currentScreen = "Dashboard" }: _Props) {
                                     lineHeight: "100%",
                                     letterSpacing: "0%"
                                 }}				
-                            >{nav.title}</Typography>
+                            >FAQs</Typography>
                         </Stack>
                     </Link>
-                ))}
-            </Box>
 
-            <Box my="auto">
-                <Typography mb={2}
-                    sx={{
-                        // fontFamily: "Libre Franklin",
-                        fontWeight: "600",
-                        fontSize: "16px",
-                        lineHeight: "100%",
-                        letterSpacing: "0%",
-                        // color: "#000"
-                    }}				
-                >Help & Support</Typography>
-
-                <Link href="/FAQs">
-                    <Stack direction="row" alignItems='center'  gap={"5px"}
-                        sx={{
-                            borderRadius: "14px",
-                            padding: "16px",
-                            mb: "5px",
-
-                            bgcolor: currentScreen == "FAQs" ? "#19A752" : "",
-                            color: currentScreen == "FAQs" ? "#fff" : '#434343',
-                            ":hover": {
-                                bgcolor: currentScreen == "FAQs" ? "" : "#19A75210",
-                            }
-                        }}
-                    >
-                        <QuestionMarkOutlinedIcon sx={{ color: "18px" }} />
-
-                        <Typography
+                    <Link href="/ContactUs">
+                        <Stack direction="row" alignItems='center'  gap={"5px"}
                             sx={{
-                                // fontFamily: "Libre Franklin",
-                                fontWeight: "400",
-                                fontSize: "16px",
-                                lineHeight: "100%",
-                                letterSpacing: "0%"
-                            }}				
-                        >FAQs</Typography>
-                    </Stack>
-                </Link>
+                                borderRadius: "14px",
+                                padding: "16px",
+                                mb: "5px",
 
-                <Link href="/ContactUs">
-                    <Stack direction="row" alignItems='center'  gap={"5px"}
-                        sx={{
-                            borderRadius: "14px",
-                            padding: "16px",
-                            mb: "5px",
+                                bgcolor: pathname.includes("/ContactUs") ? "#19A752" : "",
+                                color: pathname.includes("/ContactUs") ? "#fff" : '#434343',
+                                ":hover": {
+                                    bgcolor: pathname.includes("/ContactUs") ? "" : "#19A75210",
+                                }
+                            }}
+                        >
+                            <EmailOutlinedIcon sx={{ color: "18px" }} />
 
-                            bgcolor: currentScreen == "Contact Us" ? "#19A752" : "",
-                            color: currentScreen == "Contact Us" ? "#fff" : '#434343',
-                            ":hover": {
-                                bgcolor: currentScreen == "Contact Us" ? "" : "#19A75210",
-                            }
-                        }}
-                    >
-                        <EmailOutlinedIcon sx={{ color: "18px" }} />
+                            <Typography
+                                sx={{
+                                    // fontFamily: "Libre Franklin",
+                                    fontWeight: "400",
+                                    fontSize: "16px",
+                                    lineHeight: "100%",
+                                    letterSpacing: "0%"
+                                }}				
+                            >Contact Us</Typography>
+                        </Stack>
+                    </Link>
+                </Box>
 
-                        <Typography
-                            sx={{
-                                // fontFamily: "Libre Franklin",
-                                fontWeight: "400",
-                                fontSize: "16px",
-                                lineHeight: "100%",
-                                letterSpacing: "0%"
-                            }}				
-                        >Contact Us</Typography>
-                    </Stack>
-                </Link>
-            </Box>
-
-            <Box my="auto">
-                <LogOut />
+                <Box my="auto">
+                    <LogOut />
+                </Box>
             </Box>
         </Box>
     )
